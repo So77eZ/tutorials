b42d4ceb73f04a49e971d3f1bb5e55fde8005741
Slava <ja227@mail.ru>
2021-08-16 14:09:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// const ObjHuman = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(ObjHuman.name);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp',6];
+// console.log(arr[3]);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+// alert('hello');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?","18");
+// console.log(typeof(answer));
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?','Иван');
+// answers[1] = prompt('Как ваша фамилия?','Иванов');
+// answers[2] = prompt('Сколько вам лет?','20');
+
+// document.write(answers);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+let incr = 10,decr = 10;
+
+incr++; decr--;
+
+console.log(incr);console.log(decr);
+
+console.log(5%2);
+
+// && - И  || - ИЛИ
+
+const isCheched = true,
+      isClose = true;
+
+console.log(isCheched && isClose);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
