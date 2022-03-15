0b39f8d462bb1ce342195b132e46028b2a1affef
Slava <ja227@mail.ru>
2022-03-15 22:34:15 +0300

    Первый коммит

---
+++ js_course800$/js_course/script.js
@@ -0,0 +1,47 @@
+// // console.log(2+2);
+// 1 урок
+
+
+// "use strict";
+
+// let something = 15;
+// console.log(something);
+
+// let number = 5; //изменяема//
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj);
+// урок 2
+
+
+
+// "use strict";
+// let number = 4.6;
+// console.log(number);
+
+// const person =`Alex Clarc`;
+
+// const bool = true;
+
+// const object = {
+//     name: `John`,
+//     age:25,
+//     isMarried:false
+// };
+// console.log(object.name);
+
+// let arr = [`plum.png`,`orange.jpg`,`apple.bmp`];
+// console.log(arr[2]);
+// урок 3
+
+// "use sctrict";
+
+console.log(`Я`); console.log(`Учу`);
+console.log(`Джава скрипт епта`);
