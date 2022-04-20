0dbfac413dcc9782a932e3e54f68cf80e817fb3e
Slava <ja227@mail.ru>
2022-04-21 01:10:31 +0300

    birthday cake

--- js_course800$/js_course/script.js
+++ js_course800$/js_course/script.js
@@ -344,3 +344,25 @@
 //     result[i - 1] = data[data.length - i];
 // }
 // console.log(result);
+//9 задача
+const lines = 5;
+let result = '';
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 1; j++) {
+        result += "|";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    for (let j = 0; j < 1; j++) {
+        result += '|';
+    }
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    result += "\n";
+}
+console.log(result);
